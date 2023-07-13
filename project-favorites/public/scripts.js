6cb0eed51d9d1a433520f76732e4c624111bd0ee
joao-ressel <joao.resselti@gmail.com>
2023-07-13 15:07:25 -0500

    add front-end

--- project-favorites/public/scripts.js
+++ project-favorites/public/scripts.js
@@ -13,30 +13,40 @@ load();
 //recebe um objeto contendo o nome e a URL e cria os elementos HTML dinamicamente.
 function addElement({ name, url }) {
   const li = document.createElement('li');
+  const ul = document.getElementById("lista");
   const a = document.createElement('a');
   const trash = document.createElement('span');
   const edit = document.createElement('span'); // Elemento de edição
-  edit.className = 'editar'
+  const icon_nome = document.createElement('div');
+  const botoes = document.createElement('div'); 
+  const icon = document.createElement('a')
 
-  li.classList.add('elemento-li');
-  a.classList.add('elemento-a');
-  trash.classList.add('elemento-trash');
-  edit.classList.add('elemento-edit');
+  botoes.className = 'botoes';
+  edit.className = 'editar';
+    icon_nome.className = 'icon_nome';
+//   li.classList.add('elemento-li');
+//   a.classList.add('elemento-a');
+//   trash.classList.add('elemento-trash');
+//   edit.classList.add('elemento-edit');
 
   a.href = url;
   a.innerHTML = name;
   a.target = '_blank';
+    icon.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 31.95"><defs><style>.cls-1{fill:#009fe3;}.cls-2{fill:#1d71b8;}</style></defs><g id="Camada_2" data-name="Camada 2"><g id="Camada_1-2" data-name="Camada 1"><path class="cls-1" d="M25.46,18.14c-.26-1.56-.51-3-.71-4.44a1.14,1.14,0,0,1,.27-.81c.57-.64,1.21-1.21,1.8-1.84a4,4,0,0,0-.11-5.78,4,4,0,0,0-5.85-.1c-2.25,2.21-4.47,4.46-6.71,6.69A4.2,4.2,0,0,0,15.31,19l.58.26c-.57.53-1.07,1-1.54,1.44s-.92,1-1.36,1.46A8,8,0,0,1,10.8,9.69c2.46-2.63,5-5.17,7.65-7.64a7.86,7.86,0,0,1,10.38-.18C32.09,4.59,33,9,30.72,12.34A60.59,60.59,0,0,1,25.46,18.14Z"/><path class="cls-2" d="M16.19,12.8,19.09,10a7.91,7.91,0,0,1,3.34,10.39,8.47,8.47,0,0,1-1.58,2.22c-2.29,2.38-4.64,4.71-7,7a8,8,0,0,1-11.43-.15,7.93,7.93,0,0,1-.06-11.36C3.75,16.68,5.15,15.3,6.49,14c.26,1.49.53,3,.75,4.46a1,1,0,0,1-.31.66c-.61.65-1.26,1.24-1.87,1.89a4,4,0,0,0,.18,5.72,3.94,3.94,0,0,0,5.71.22q3.66-3.54,7.2-7.2A4.1,4.1,0,0,0,17,13.18C16.73,13.05,16.49,13,16.19,12.8Z"/></g></g></svg>';
+  
 
-  trash.innerHTML = 'Excluir';
+  trash.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" height="48" viewBox="0 -960 960 960" width="48"><path d="M261-120q-24.75 0-42.375-17.625T201-180v-570h-41v-60h188v-30h264v30h188v60h-41v570q0 24-18 42t-42 18H261Zm438-630H261v570h438v-570ZM367-266h60v-399h-60v399Zm166 0h60v-399h-60v399ZM261-750v570-570Z"/></svg>';
   trash.onclick = () => removeElement(li, { name, url });
 
-  edit.innerHTML = 'Editar'; // Texto do elemento de edição
+  edit.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" height="48" viewBox="0 -960 960 960" width="48"><path d="M180-180h44l443-443-44-44-443 443v44Zm614-486L666-794l42-42q17-17 42-17t42 17l44 44q17 17 17 42t-17 42l-42 42Zm-42 42L248-120H120v-128l504-504 128 128Zm-107-21-22-22 44 44-22-22Z"/></svg>'; // Texto do elemento de edição
   edit.onclick = () => editElement({ name, url }); // Chamar a função de edição
 
   ul.append(li);
-  li.append(a);
-  li.append(edit); // Adicionar o elemento de edição ao item
-  li.append(trash);
+  icon_nome.append(icon, a);
+  li.append(icon_nome, botoes);
+  
+  botoes.append(edit); // Adicionar o elemento de edição ao item
+  botoes.append(trash);
 }
 
 // recebe um objeto contendo o nome e a URL 
